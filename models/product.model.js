7797d4cf140ed86f73ca27f5ca604cf608f1f702
XQuynh3 <xuanquynh3824@gmail.com>
2025-12-02 16:27:17 +0700

    change url in swagger

--- models/product.model.js
+++ models/product.model.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true, trim: true },
-    price: { type: Number, required: true },
-    description: { type: String, default: "" },
-    image: { type: String, default: "" },
-    category: { type: String, default: "" },
-    stock: { type: Number, default: 0 }
-  },
-  { timestamps: true }
-);
+// const productSchema = new mongoose.Schema(
+//   {
+//     name: { type: String, required: true, trim: true },
+//     price: { type: Number, required: true },
+//     description: { type: String, default: "" },
+//     image: { type: String, default: "" },
+//     category: { type: String, default: "" },
+//     stock: { type: Number, default: 0 }
+//   },
+//   { timestamps: true }
+// );
 
-module.exports = mongoose.model("Product", productSchema);
+// module.exports = mongoose.model("Product", productSchema);
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const router = express.Router();
-const {
-  getAllProducts,
-  getProductById
-} = require("../controllers/product.controller");
+// const express = require("express");
+// const router = express.Router();
+// const {
+//   getAllProducts,
+//   getProductById
+// } = require("../controllers/product.controller");
 
-// public
-router.get("/", getAllProducts);
-router.get("/:id", getProductById);
+// // public
+// router.get("/", getAllProducts);
+// router.get("/:id", getProductById);
 
-module.exports = router;
+// module.exports = router;
